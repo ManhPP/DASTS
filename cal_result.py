bc1a95e9a811034edb64dd8f8da585c7a6bebf7a
manhpp <phumanh1998@gmail.com>
2021-08-06 16:20:23 +0700

    update

--- cal_result.py
+++ cal_result.py
@@ -1,10 +1,10 @@
 import argparse
 import glob
-from datetime import datetime
 
 from omegaconf import OmegaConf
 
 from src.util import *
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='DASTS')
@@ -13,14 +13,10 @@ if __name__ == '__main__':
                         help='path of config file')
     args = parser.parse_args()
     config = OmegaConf.load(args.config)
-    config.result_folder = os.path.join(config.result_folder, datetime.now().strftime("%m%d%Y%H%M%S"))
     paths = glob.glob(config.data_path)
     for data_set in paths:
         print(data_set)
         inp = load_input(config, data_set)
-        if config.params.cus_per_staff > 0:
-            config.params.num_staff = int(np.ceil(inp['num_cus'] / config.params.cus_per_staff))
-
         tau = inp["tau"]
         tau_a = inp["tau_a"]
         num_cus = inp["num_cus"]
