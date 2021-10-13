83130dd2eb0ed0fca25664ee3be0817877b44efa
manhpp <phumanh1998@gmail.com>
2021-10-14 00:09:47 +0700

    update data path

--- main.py
+++ main.py
@@ -18,19 +18,21 @@ if __name__ == '__main__':
     args = parser.parse_args()
     config = OmegaConf.load(args.config)
     config.result_folder = os.path.join(config.result_folder, datetime.now().strftime("%m%d%Y%H%M%S"))
-    paths = glob.glob(config.data_path)
-    print(paths)
-    for data_set in paths:
-        print(data_set)
-        try:
-            inp = load_input(config, data_set)
-            if config.params.cus_per_staff > 0:
-                config.params.num_staff = int(np.ceil(inp['num_cus']/config.params.cus_per_staff))
-            if config.solver.solver == "GUROBI":
-                solve_by_gurobi(config, inp)
-            elif config.solver.solver == "CPLEX":
-                solve_by_cplex(config, inp)
-            else:
-                solve_by_ortools(config, inp)
-        except Exception as e:
-            print("Error: ", e)
+
+    for data_path in config.data_path.split(","):
+        paths = glob.glob(data_path)
+        print(paths)
+        for data_set in paths:
+            print(data_set)
+            try:
+                inp = load_input(config, data_set)
+                if config.params.cus_per_staff > 0:
+                    config.params.num_staff = int(np.ceil(inp['num_cus']/config.params.cus_per_staff))
+                if config.solver.solver == "GUROBI":
+                    solve_by_gurobi(config, inp)
+                elif config.solver.solver == "CPLEX":
+                    solve_by_cplex(config, inp)
+                else:
+                    solve_by_ortools(config, inp)
+            except Exception as e:
+                print("Error: ", e)
